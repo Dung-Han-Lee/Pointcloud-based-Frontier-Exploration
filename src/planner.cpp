a091cf2f069098f87ba9f4c8f1c15283794fe366
Dung-Han-Lee <dhl77771@gmail.com>
2020-04-14 12:48:10 -0400

    add convex hull to ros

--- src/planner.cpp
+++ src/planner.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include "convexhull.h"
 
 class Master{
   public:
@@ -7,7 +8,7 @@ class Master{
 
     ~Master() = default;
     
-    void VizPolyhedron();
+    void VizPolyhedron(const std::vector<Face>& triangles) const;
 
     int Loop();
 
@@ -18,23 +19,28 @@ class Master{
 };
 
 Master::Master(ros::NodeHandle& nh){
-  convex_pub = nh.advertise<visualization_msgs::Marker>("convex hull", 10);
+  convex_pub = nh.advertise<visualization_msgs::Marker>("convex_hull", 10);
 }
 
 int Master::Loop(){
   while(ros::ok())
   {
+    std::vector<std::vector<int>> tests = {{0,0,0}, {0,3,0}, {3,0,0}, {0,0,3}};
+    std::vector<Vec3> vecs;
+    std::vector<Face> faces;
+    for(auto test : tests) vecs.emplace_back(test[0], test[1], test[2]);
+    ConvexHull c(vecs);
     ros::Rate loop_rate(10);
-    this->VizPolyhedron();
+    this->VizPolyhedron(c.GetFaces());
     ros::spinOnce();
     loop_rate.sleep();
   }    
 }
 
-void Master::VizPolyhedron()
+void Master::VizPolyhedron(const std::vector<Face>& triangles) const
 {
   visualization_msgs::Marker scan_marker;
-  scan_marker.header.frame_id = "/robot_0/base_laser_link";
+  scan_marker.header.frame_id = "map";
   scan_marker.header.stamp = ros::Time::now();
   scan_marker.ns = "scan";
   scan_marker.type = visualization_msgs::Marker::TRIANGLE_LIST;
@@ -49,20 +55,32 @@ void Master::VizPolyhedron()
   scan_marker.pose.orientation.y = 0.0;
   scan_marker.pose.orientation.z = 0.0;
   scan_marker.pose.orientation.w = 1.0;
-  scan_color.a = 0.8f;
+  scan_color.a = 0.5f;
   scan_color.r = 0.25f;
-  scan_color.g = 0.5f;
+  scan_color.g = 0.9f;
   scan_color.b = 0.9f;
   scan_marker.color = scan_color;
   geometry_msgs::Point temp;
 
-  std::vector<std::vector<double>> points = {{0,0,0}, {11.,0,0}, {0,10,0}};
-  for (int i = 0; i < points.size(); i++) {
-    temp.x = points[i][0];
-    temp.y = points[i][1];
-    temp.z = points[i][2];
+  auto viz_one_point = [&](const Vec3& p)
+  {
+    temp.x = p.x; temp.y = p.y; temp.z = p.z;
     scan_marker.points.push_back(temp);
+  };
+
+  for (const auto& triangle : triangles) 
+  {
+    viz_one_point(triangle.a);
+    viz_one_point(triangle.b);
+    viz_one_point(triangle.c);
   }
+
   this->convex_pub.publish(scan_marker);
 }
 
+int main(int argc, char** argv){
+  ros::init(argc, argv, "exploration_node");
+  ros::NodeHandle nh;
+  Master master(nh);
+  master.Loop();
+}
